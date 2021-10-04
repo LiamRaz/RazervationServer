29d2a57c72a239d20137644e652c398ab187aa01
User <User@LAB2-14>
2021-10-04 14:42:35 +0300

    a little bit more work

---
+++ RazervationServerBL/ModelsBL/RazervationDBContextBL.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazervationServerBL.ModelsBL
+{
+    partial class RazervationDBContextBL: DbContext
+    {
+        public User Login(string email, string pswd)
+        {
+            User user = this.Users
+                .Include(us => us.UserContacts)
+                .ThenInclude(uc => uc.ContactPhones)
+                .Where(u => u.Email == email && u.UserPswd == pswd).FirstOrDefault();
+
+            return user;
+        }
+    }
+}
